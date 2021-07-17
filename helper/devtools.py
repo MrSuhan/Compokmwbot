641e750dffb645895969efcf03794b32c291b92d
MrSuhan <86226614+MrSuhan@users.noreply.github.com>
2021-07-17 12:44:42 +0530

    Update devtools.py

--- helper/devtools.py
+++ helper/devtools.py
@@ -18,7 +18,7 @@ from .stuff import *
 async def eval(event):
     if event.sender_id != OWNER:
         return
-    await event.reply("Processing ...")
+    await event.reply("Processing😋 ...")
     cmd = event.text.split(" ", maxsplit=1)[1]
     old_stderr = sys.stderr
     old_stdout = sys.stdout
